1ac2d49dce1aa0f571a7b6df3c313230f263caea
Rafael S <rafael.sabrantes@senacsp.edu.br>
2021-08-31 20:04:27 -0300

    Coloquei a mensagem de bom dia

--- Projeto teste 2/Program.cs
+++ Projeto teste 2/Program.cs
@@ -7,6 +7,7 @@ namespace Projeto_teste_2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia jovem!");
         }
     }
 }
