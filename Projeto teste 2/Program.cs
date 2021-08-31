d238bb5c933bc7383eda99125a5cd951e9e608a1
Rafael S <rafael.sabrantes@senacsp.edu.br>
2021-08-31 20:01:43 -0300

    Projeto iniciado, mostrando a mensagem ola mundo

---
+++ Projeto teste 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projeto_teste_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
